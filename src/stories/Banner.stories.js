cdc65dd89b7b441023fc88baa0fa5483dacf8a32
Tom Russell <TR115251@hotmail.co.uk>
2020-11-27 16:59:18 +0000

    jac-kit/search-filter

--- src/stories/Banner.stories.js
+++ src/stories/Banner.stories.js
@@ -1,21 +1,26 @@
-import MyBanner from '../packages/components/Banner/Banner';
+import Banner from "../packages/components/Banner/Banner";
 
 export default {
-  title: 'MOJ-Kit/Banner',
-  component: MyBanner,
+  title: "JAC-Kit/Banner",
+  component: Banner,
   argTypes: {
-    status: { control: { type: 'select', options: ['success', 'warning', 'information'] } }
-  },
+    status: {
+      control: {
+        type: "select",
+        options: ["success", "warning", "information"]
+      }
+    }
+  }
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { MyBanner },
-  template: '<MyBanner v-bind="$props" />',
+  components: { Banner },
+  template: '<Banner v-bind="$props" />'
 });
 
 export const Types = Template.bind({});
 Types.args = {
-  status: 'success',
-  message: 'My initial message - pls test without message'
+  status: "success",
+  message: "My initial message - pls test without message"
 };
---
+++ src/stories/SearchFilter/SearchFilter.stories.js
@@ -0,0 +1,26 @@
+// import SearchFilter from "../packages/components/Banner/Banner";
+
+// export default {
+//   title: "JAC-Kit/SearchFilter",
+//   component: SearchFilter,
+//   argTypes: {
+//     status: {
+//       control: {
+//         type: "select",
+//         options: ["success", "warning", "information"]
+//       }
+//     }
+//   }
+// };
+
+// const Template = (args, { argTypes }) => ({
+//   props: Object.keys(argTypes),
+//   components: { SearchFilter },
+//   template: '<SearchFilter v-bind="$props" />'
+// });
+
+// export const Types = Template.bind({});
+// Types.args = {
+//   status: "success",
+//   message: "My initial message - pls test without message"
+// };
