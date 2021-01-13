21a66ef0c725949db45b644b16776d5803de7539
Tom Russell <TR115251@hotmail.co.uk>
2021-01-13 11:14:51 +0000

    small screen tables

---
+++ src/stories/components/Table/Badge.stories.js
@@ -0,0 +1,18 @@
+import Badge from '../../../packages/components/Table/Badge';
+
+export default {
+  title: 'JAC-Kit/Badge',
+  component: Badge,
+  argTypes: 'number',
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Badge },
+  template: '<Badge v-bind="$props" />',
+});
+
+export const Types = Template.bind({});
+Types.args = {
+  number: 10,
+};
