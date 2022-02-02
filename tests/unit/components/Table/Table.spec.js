cba685bf907cfcd3b0b0ba58e12d18fc886e0048
Tom Russell <TR115251@hotmail.co.uk>
2022-02-02 15:36:30 +0000

    tests

---
+++ tests/unit/components/Table/Table.spec.js
@@ -0,0 +1,40 @@
+import { createTestSubject } from '../../helpers';
+
+import Table from '@/components/Table/Table';
+
+describe('components/Banner', () => {
+  describe('props', () => {
+    let prop;
+    describe('message', () => {
+      beforeEach(() => {
+        prop = Table.props.columns;
+      });
+      it('is a string', () => {
+        expect(prop.type()).toBeArray();
+      });
+    });
+  });
+  
+  describe('component instance', () => {
+    let wrapper;
+      beforeEach(() => {
+      wrapper = createTestSubject(Table, {
+        mocks: {},
+        stubs: [],
+        propsData: {
+          columns: [],
+          data: [],
+          dataKey: 'mockDataKey',
+        },
+      });
+    });
+      
+    it('renders the component', () => {
+      expect(wrapper.exists()).toBe(true);
+    });
+    
+    it('displays message when data is empty', () => {
+      expect(wrapper.find('div .govuk-body').text()).toBe('No results found');
+    });
+  });
+});
