a1636199375ce0ac3ddc308d394dfd321c2fab61
Tom Russell <TR115251@hotmail.co.uk>
2022-04-25 17:04:59 +0100

    errors not clearing fixed

--- src/packages/draftComponents/Form/Form.vue
+++ src/packages/draftComponents/Form/Form.vue
@@ -21,21 +21,19 @@ export default {
       this.$root.$emit('validate');
     },
     handleError(payload) {
-      if (payload.message) {
-        this.errorObject[payload.id] = payload.message;
-        this.errors = []; // re-populate errors
-        for (const item in this.errorObject) {
-          if (this.errorObject[item]) {
-            this.errors.push({ id: item, message: this.errorObject[item] });
-          }
+      this.errorObject[payload.id] = payload.message;
+      this.errors = []; // re-populate errors
+      for (const item in this.errorObject) {
+        if (this.errorObject[item]) {
+          this.errors.push({ id: item, message: this.errorObject[item] });
         }
-        if (this.errors.length) {
-          this.scrollToErrorSummary();
-        }
+      }
+      if (this.errors.length) {
+        this.scrollToErrorSummary();
       }
     },
     scrollToErrorSummary(){
-      //I think this is just scrolling to top of page
+      //This is just scrolling to top of page
       this.$root.$el.scrollIntoView();
     },
     isValid() {
