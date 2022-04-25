34b64345c8dc7e97746f4c03b3bdc73a57197f99
Tom Russell <TR115251@hotmail.co.uk>
2022-04-25 15:34:13 +0100

    scrollIntoView in Form

--- src/packages/draftComponents/Form/Form.vue
+++ src/packages/draftComponents/Form/Form.vue
@@ -21,13 +21,22 @@ export default {
       this.$root.$emit('validate');
     },
     handleError(payload) {
-      this.errorObject[payload.id] = payload.message;
-      this.errors = []; // re-populate errors
-      for (const item in this.errorObject) {
-        if (this.errorObject[item]) {
-          this.errors.push({ id: item, message: this.errorObject[item] });
+      if (payload.message) {
+        this.errorObject[payload.id] = payload.message;
+        this.errors = []; // re-populate errors
+        for (const item in this.errorObject) {
+          if (this.errorObject[item]) {
+            this.errors.push({ id: item, message: this.errorObject[item] });
+          }
+        }
+        if (this.errors.length) {
+          this.scrollToErrorSummary();
         }
       }
+    },
+    scrollToErrorSummary(){
+      //I think this is just scrolling to top of page
+      this.$root.$el.scrollIntoView();
     },
     isValid() {
       return this.errors.length === 0;
