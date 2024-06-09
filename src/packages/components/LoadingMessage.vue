8c7f1ef3fc01385ae28aad524c82dc7f59705ba5
drieJac <jac.drie@digitalteam.uk>
2024-06-10 00:19:14 +0100

    Extract the spinner into its own component so it can be used separately from the LoadingMessage

--- src/packages/components/LoadingMessage.vue
+++ src/packages/components/LoadingMessage.vue
@@ -3,10 +3,7 @@
     <div
       v-if="!loadFailed"
     >
-      <span
-        ref="loadingMessage"
-        class="spinner-border spinner-border-sm" 
-      />
+      <Spinner style="vertical-align: text-bottom" />
       Loading...
     </div>
     <div
@@ -24,43 +21,13 @@
 </template>
 <!-- todo: remove this note (here re:change detection) -->
 <script>
+import Spinner from './Spinner.vue';
 export default {
+  components: {
+    Spinner,
+  },
   props: {
     loadFailed: Boolean,
   },
 };
 </script>
-
-<style scoped>
-  @-webkit-keyframes spinner-border {
-    to {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
-
-  @keyframes spinner-border {
-    to {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
-
-  .spinner-border {
-    display: inline-block;
-    width: 2rem;
-    height: 2rem;
-    vertical-align: text-bottom;
-    border: 0.25em solid currentColor;
-    border-right-color: transparent;
-    border-radius: 50%;
-    -webkit-animation: spinner-border .75s linear infinite;
-    animation: spinner-border .75s linear infinite;
-  }
-
-  .spinner-border-sm {
-    width: 1rem;
-    height: 1rem;
-    border-width: 0.2em;
-  }
-</style>
---
+++ src/packages/components/Spinner.vue
@@ -0,0 +1,37 @@
+<template>
+  <span
+    class="spinner-border spinner-border-sm"
+  />
+</template>
+<style scoped>
+  @-webkit-keyframes spinner-border {
+    to {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes spinner-border {
+    to {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+
+  .spinner-border {
+    display: inline-block;
+    width: 2rem;
+    height: 2rem;
+    border: 0.25em solid currentColor;
+    border-right-color: transparent;
+    border-radius: 50%;
+    -webkit-animation: spinner-border .75s linear infinite;
+    animation: spinner-border .75s linear infinite;
+  }
+
+  .spinner-border-sm {
+    width: 1rem;
+    height: 1rem;
+    border-width: 0.2em;
+  }
+</style>
