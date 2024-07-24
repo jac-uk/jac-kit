88129831ff5d6e5409a99aabfed3d26f6a14f310
halcyonjac <halcyon@judicialappointments.digital>
2024-07-24 13:46:27 +0100

    update table filters

--- src/packages/components/Table/Table.vue
+++ src/packages/components/Table/Table.vue
@@ -525,14 +525,20 @@ export default {
       this.direction = this.defaultState.direction;
     }
     this.initialiseFilters();
-    this.changeTableState(ACTIONS.LOAD, this.currentState);
+
+    // check if there are any default filters
+    if (Object.keys(this.filterValues).length) {
+      this.btnUpdateFilters();
+    } else {
+      this.changeTableState(ACTIONS.LOAD, this.currentState);
+    }
   },
   methods: {
     initialiseFilters() {
       for (const filter of this.filters) {
         if ('defaultValue' in filter) {
-          // Only supports radio currently (others can be supported as and when they're needed)
-          if (filter.type === 'radio') {
+          // Only supports radio and checkbox currently (others can be supported as and when they're needed)
+          if (['radio', 'checkbox'].includes(filter.type)) {
             this.filterValues[filter.field] = filter.defaultValue;
           }
         }
@@ -696,6 +702,7 @@ export default {
           }
           break;
         case 'radio':
+        case 'option':
           if (this.filterValues[filter.field]) {
             const value = this.filterValues[filter.field];
             // Ensure if empty option is selected the 'where' clause is skipped
