7a7286bf15ee22b4b4b49d7a7a6b72a6925aa47b
Tom Russell <TR115251@hotmail.co.uk>
2024-08-29 16:37:59 +0100

    finding missing helpers

--- src/packages/helpers/constants.js
+++ src/packages/helpers/constants.js
@@ -63,6 +63,38 @@ const EXERCISE_STAGE = {
   SELECTABLE: 'selectable', // to be removed
 };
 
+const APPLICATION_FORM_PARTS = {
+  PERSONAL_DETAILS: 'personalDetails',
+  CHARACTER_INFORMATION: 'characterInformation',
+  EQUALITY_AND_DIVERSITY_SURVEY: 'equalityAndDiversitySurvey',
+  PART_TIME_WORKING_PREFERENCES: 'partTimeWorkingPreferences',
+  LOCATION_PREFERENCES: 'locationPreferences',
+  JURISDICTION_PREFERENCES: 'jurisdictionPreferences',
+  ADDITIONAL_WORKING_PREFERENCES: 'additionalWorkingPreferences',
+  WELSH_POSTS: 'welshPosts',
+  RELEVANT_QUALIFICATIONS: 'relevantQualifications',
+  RELEVANT_MEMBERSHIPS: 'relevantMemberships',
+  POST_QUALIFICATION_WORK_EXPERIENCE: 'postQualificationWorkExperience',
+  JUDICIAL_EXPERIENCE: 'judicialExperience',
+  RELEVANT_EXPERIENCE: 'relevantExperience',
+  EMPLOYMENT_GAPS: 'employmentGaps',
+  REASONABLE_LENGTH_OF_SERVICE: 'reasonableLengthOfService',
+  ASSESSORS_DETAILS: 'assessorsDetails',
+  LEADERSHIP_JUDGE_DETAILS: 'leadershipJudgeDetails',
+  STATEMENT_OF_SUITABILITY: 'statementOfSuitability',
+  COVERING_LETTER: 'coveringLetter',
+  CV: 'cv',
+  STATEMENT_OF_ELIGIBILITY: 'statementOfEligibility',
+  SELF_ASSESSMENT_COMPETENCIES: 'selfAssessmentCompetencies',
+  ADDITIONAL_INFO: 'additionalInfo',
+  CANDIDATE_AVAILABILITY: 'candidateAvailability',
+  PANEL_CONFLICTS: 'panelConflicts',
+  COMMISSIONER_CONFLICTS: 'commissionerConflicts',
+  CHARACTER_CHECKS: 'characterChecks',
+  REASONABLE_ADJUSTMENTS: 'reasonableAdjustments',
+  WORKING_PREFERENCES: 'workingPreferences',
+};
+
 const APPLICATION_STATUS = {
   // v2
   CRITICAL_ANALYSIS_PASSED: 'criticalAnalysisPassed',
@@ -234,6 +266,13 @@ const NOTIFICATIONS = {
   FINAL_SELECTION: 'finalSelection',
 };
 
+const WORKING_BASIS = {
+  FULL_TIME: 'full-time',
+  SALARIED_PART_TIME: 'salaried-part-time',
+  FEE_PAID: 'fee-paid',
+  VOLUNTARY: 'voluntary',
+};
+
 export {
   STATUS,
   EXERCISE_STAGE,
@@ -249,5 +288,7 @@ export {
   NOTIFICATIONS,
   TASK_TYPE,
   SHORTLISTING_TASK_TYPES,
-  ASSESSMENT_METHOD
+  ASSESSMENT_METHOD,
+  APPLICATION_FORM_PARTS,
+  WORKING_BASIS
 };
