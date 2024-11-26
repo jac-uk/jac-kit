763dfbf5810160bbad2d64b892e18c9db7d8cc4c
Tom Russell <TR115251@hotmail.co.uk>
2024-11-26 18:27:08 +0000

    sanitise

--- src/packages/helpers/export.js
+++ src/packages/helpers/export.js
@@ -1,6 +1,14 @@
 import XLSXPopulate from 'xlsx-populate';
 import { save } from 'save-file';
 
+// Sanitisation function
+const sanitiseForExcel = (input) => {
+  if (input && /^[=+\-@]/.test(input)) {
+    return `'${input}`;
+  }
+  return input;
+};
+
 const downloadXLSX = async (data, options) => {
   const workbook = await XLSXPopulate.fromBlankAsync();
 
@@ -11,12 +19,16 @@ const downloadXLSX = async (data, options) => {
 
   const sheet = workbook.sheet(0);
 
-  /* NOTE:
-   Sheet name length should not be more than 31 and not contain special characters
-   */
+  // NOTE: Sheet name length should not be more than 31 and not contain special characters
   sheet.name(options.sheetName.replace(/[^\w\s-]/gi, '').substring(0, 30));
 
-  sheet.cell('A1').value(data);
+  // Sanitize each value in the data array before adding to the sheet
+  const sanitizedData = data.map(row => {
+    return row.map(cell => sanitiseForExcel(cell)); 
+  });
+
+  // Apply sanitized data to the sheet starting at A1
+  sheet.cell('A1').value(sanitizedData);
   sheet.row(1).style({
     bold: true,
     fill: 'eeeeee',
